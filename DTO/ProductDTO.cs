5628bf99204a6055fab526527a5ee78e34511c2c
Yanis Hermassi <yanis.hermassi@gmail.com>
2021-03-11 00:34:16 +0000

    remove trash files

--- DTO/ProductDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace Shop.DTO
-{
-    public class ProductDTO
-    {
-        public int product_id { get; set; }
-        public string product_name { get; set; }
-        public decimal price { get; set; }
-    }
-}
